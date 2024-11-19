62ee3bf0e6d6d3b283c7ccf65db12079a44d771b
anaines14 <n@chos.rocks>
2024-11-19 22:53:03 +0000

    we go 2

--- scripts/auto-gammarelay.sh
+++ scripts/auto-gammarelay.sh
@@ -2,10 +2,17 @@
 
 while true; do
   hours="$(date +'%H')"
+  next_proc=5
   if [ "$hours" -ge 21 ] || [ "$hours" -lt 8 ]; then
+    next_proc=$(((24 - hours + 8 - 1) * 60 + 5))
+    busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 3000
+  elif [ "$hours" -lt 8 ]; then
+    next_proc=$(((8 - hours - 1) * 60 + 5))
     busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 3000
   else
+    next_proc=$(((21 - hours - 1) * 60 + 5))
     busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500
   fi
-  sleep 5m
+  echo "Will trigger again in ${next_proc}m"
+  sleep "${next_proc}m"
 done
